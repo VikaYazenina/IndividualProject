029b9ce4f9655793f4391fdb070e321a55552dff
VikaYazenina <vikayazenina@list.ru>
2026-09-24 14:39:44 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
  
-from models import Base
+from .models import Base
  
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///aggregator.db")
  
